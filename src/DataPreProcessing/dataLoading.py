dc04105657c4b9727acab134c66b115e7dcfe708
kevin-joseph05 <kevin.joseph542@gmail.com>
2025-11-07 23:40:08 -0500

    implemented load_csv and export_csv

--- src/DataPreProcessing/dataLoading.py
+++ src/DataPreProcessing/dataLoading.py
@@ -1,11 +1,26 @@
 # loads from mongo db
+from pymongo import MongoClient
+import csv
+import pandas as pd
 
 class Data:
 
     # mongo db document passed in here
-    def __init__(self):
-        pass
-    
+    def __init__(self) -> None:
+        self.client = MongoClient("mongodb://root:password@localhost:27017/")
+        self.db = self.client["test_database"]
+        self.items_collection = self.db["collection1"]
+
     # export back to mongo db
-    def export_csv():
-        pass
+    def load_csv(self, csv_file: csv) -> None:
+        with open(csv_file, newline='') as f:
+            reader = csv.DictReader(f)
+            data = list(reader)
+        self.items_collection.insert_many(data)
+
+    def export_csv(self, collection_name: str) -> None:
+        collection = self.db[collection_name]
+        data = list(collection.find())
+        df = pd.DataFrame(data)
+        df.to_csv(f"{collection_name}.csv")
+
